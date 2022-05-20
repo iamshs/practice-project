101dd51e05033cb98abb8e64cb15e6afa5eddc32
iamshs <mdsolayman998@gmail.com>
2022-05-21 03:26:43 +0600

    footer implement

--- src/Pages/Home/Footer.js
+++ src/Pages/Home/Footer.js
@@ -2,28 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <footer class="footer p-10 bg-neutral text-neutral-content">
-        <div>
-          <span class="footer-title">Services</span> 
-          <Link to={'/'} class="link link-hover">Branding</Link>
-          <Link to={'/'} class="link link-hover">Design</Link>
-          <Link to={'/'} class="link link-hover">Marketing</Link>
-          <Link to={'/'} class="link link-hover">Advertisement</Link>
+        <footer className='bg-neutral'>
+      <div className="footer p-10  text-neutral-content">
+      <div>
+          <span className="footer-title">Services</span> 
+          <Link to={'/'} className="link link-hover">Branding</Link>
+          <Link to={'/'} className="link link-hover">Design</Link>
+          <Link to={'/'} className="link link-hover">Marketing</Link>
+          <Link to={'/'} className="link link-hover">Advertisement</Link>
         </div> 
         <div>
-          <span class="footer-title">Company</span> 
-          <Link to={'/'} class="link link-hover">About us</Link>
-          <Link to={'/'} class="link link-hover">Contact</Link>
-          <Link to={'/'} class="link link-hover">Jobs</Link>
-          <Link to={'/'} class="link link-hover">Press kit</Link>
+          <span className="footer-title">Company</span> 
+          <Link to={'/'} className="link link-hover">About us</Link>
+          <Link to={'/'} className="link link-hover">Contact</Link>
+          <Link to={'/'} className="link link-hover">Jobs</Link>
+          <Link to={'/'} className="link link-hover">Press kit</Link>
         </div> 
         <div>
-          <span class="footer-title">Legal</span> 
-          <Link to={'/'} class="link link-hover">Terms of use</Link>
-          <Link to={'/'} class="link link-hover">Privacy policy</Link>
-          <Link to={'/'} class="link link-hover">Cookie policy</Link>
+          <span className="footer-title">Legal</span> 
+          <Link to={'/'} className="link link-hover">Terms of use</Link>
+          <Link to={'/'} className="link link-hover">Privacy policy</Link>
+          <Link to={'/'} className="link link-hover">Cookie policy</Link>
         </div>
+      </div>
+        <p className='text-center text-white'> <span dangerouslySetInnerHTML={{ "__html": "&copy;" }} /> {year} Wild Life</p>
       </footer>
     );
 };
